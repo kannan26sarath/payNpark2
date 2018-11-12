f020b40db4a70a861ba736f48fcaa3eca5462e4f
SJCET <sjcet@palai>
2018-11-11 23:05:10 -0800

    b

--- app/src/main/java/com/example/kanna/paynpark/AboutActivity.java
+++ app/src/main/java/com/example/kanna/paynpark/AboutActivity.java
@@ -2,7 +2,9 @@ package com.example.kanna.paynpark;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -11,5 +13,22 @@ public class AboutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
+        sendSMS("9072948997","hai this is text");
+
+
+
+    }
+
+    public void sendSMS(String phoneNo, String msg) {
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(phoneNo, null, msg, null, null);
+            Toast.makeText(getApplicationContext(), "Message Sent",
+                    Toast.LENGTH_LONG).show();
+        } catch (Exception ex) {
+            Toast.makeText(getApplicationContext(),ex.getMessage().toString(),
+                    Toast.LENGTH_LONG).show();
+            ex.printStackTrace();
+        }
     }
 }

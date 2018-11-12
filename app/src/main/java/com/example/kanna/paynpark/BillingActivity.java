ae53cc599c04f2b1d1d8d2582fced79e18bd34fd
SJCET <sjcet@palai>
2018-11-12 02:23:20 -0800

    b

--- app/src/main/java/com/example/kanna/paynpark/BillingActivity.java
+++ app/src/main/java/com/example/kanna/paynpark/BillingActivity.java
@@ -3,6 +3,11 @@ package com.example.kanna.paynpark;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -24,68 +29,93 @@ public class BillingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_billing);
 
+        final EditText txtparkno=findViewById(R.id.txtparkid);
+        final TextView txtvno=findViewById(R.id.txtvno);
+        Button btnCaluclate=findViewById(R.id.buttonCalculate);
+        btnCaluclate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-        try {
-            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-            StrictMode.setThreadPolicy(policy);
 
-            OkHttpClient client = new OkHttpClient();
 
-            HttpUrl.Builder urlBuilder = HttpUrl.parse("http://192.168.1.1/CRUDAPI/read.php").newBuilder();
-           // urlBuilder.addQueryParameter("PID", txtPID.getText().toString());
 
-            String url = urlBuilder.build().toString();
+                try {
+                    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                    StrictMode.setThreadPolicy(policy);
 
-            Request request = new Request.Builder()
-                    .url(url)
-                    .build();
+                    OkHttpClient client = new OkHttpClient();
 
-            client.newCall(request).enqueue(new Callback() {
-                @Override
-                public void onFailure(Call call, IOException e) {
+                    HttpUrl.Builder urlBuilder = HttpUrl.parse("http://117.193.161.207/17lemca049/database/payment.php").newBuilder();
+                    urlBuilder.addQueryParameter("park_id", String.valueOf( txtparkno.getText()));
 
-                }
+                    String url = urlBuilder.build().toString();
 
-                @Override
-                public void onResponse(Call call, final Response response) throws IOException {
+                    Request request = new Request.Builder()
+                            .url(url)
+                            .build();
 
-                    runOnUiThread(new Runnable() {
+                    client.newCall(request).enqueue(new Callback() {
                         @Override
-                        public void run() {
-
-                            try {
-                                //txtInfo.setText(response.body().string());
-
-                                try {
-                                    String data = response.body().string();
-
-                                    JSONArray jsonArray = new JSONArray(data);
-                                    JSONObject jsonObject;
-
-                                    jsonObject = jsonArray.getJSONObject(0);
-
-                                   // txtPID.setText(jsonObject.getString("ProductID"));
-                                   // txtPName.setText(jsonObject.getString("PName"));
-                                    //txtPrice.setText(jsonObject.getString("Price"));
-
-                                } catch (JSONException e) {
-                                    //txtInfo.setText(e.getMessage());
-                                }
-
-
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
+                        public void onFailure(Call call, IOException e) {
 
                         }
+
+                        @Override
+                        public void onResponse(Call call, final Response response) throws IOException {
+
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+
+                                    try {
+                                        //txtInfo.setText(response.body().string());
+
+                                        try {
+                                            String data = response.body().string();
+
+                                            JSONArray jsonArray = new JSONArray(data);
+                                            JSONObject jsonObject;
+
+                                            jsonObject = jsonArray.getJSONObject(0);
+
+                                            txtvno.setText(jsonObject.getString("park_vehno"));
+                                            // txtPName.setText(jsonObject.getString("PName"));
+                                            //txtPrice.setText(jsonObject.getString("Price"));
+
+                                        } catch (JSONException e) {
+                                            //txtInfo.setText(e.getMessage());
+                                            Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_LONG).show();
+                                        }
+
+
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+
+                                }
+                            });
+                        }
+
+                        ;
                     });
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
 
-                ;
-            });
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+
+
+
+
+
+
+
+
+
+            }
+        });
+
+
+
 
 
     }

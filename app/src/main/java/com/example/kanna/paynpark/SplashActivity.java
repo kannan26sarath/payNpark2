bbb4ef8b7d94ef0817f40476e94587dbc9f22b37
kannan26sarath <kannan26sarath@gmail.com>
2018-11-12 11:43:55 +0530

    logo set

---
+++ app/src/main/java/com/example/kanna/paynpark/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.kanna.paynpark;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static int TIME_OUT = 3000;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        final View myLayout = findViewById(R.id.startscreen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, HomePage.class);
+                startActivity(i);
+                finish();
+            }
+        }, TIME_OUT);
+    }
+}

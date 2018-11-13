a8463c9f365029a2076cb70543e597bd4e6511fa
kannan26sarath <kannan26sarath@gmail.com>
2018-11-13 11:02:52 +0530

    fare update

---
+++ database/fareupdate.php
@@ -0,0 +1,23 @@
+
+<?php
+
+$conn = mysqli_connect("localhost","17lemca049", "1174", "17lemca049");
+
+
+$park_catgry=$_GET['park_catgry'];
+$park_amount=$_GET['park_amount'];
+
+$query = "UPDATE tbl_fair SET `amount`=$park_amount WHERE `catgry`='$catgry`';";
+
+if(mysqli_query($conn, $query))
+{
+     echo "success";
+}
+else
+{
+     echo "failed";
+}
+
+mysqli_close($conn);
+
+?>
